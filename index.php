1660ad3c5e13433ed0bb7db5c3a90eb71918d668
yanwu <yanwu@qinsilk.com>
2020-09-19 17:45:50 +0800

    初始化

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main template file
+ */
+?>
+<?php 
+	// 头部区域
+	get_header();
+?>
+
+<div class="nav">
+	<div class="content-box">
+		<?php
+			// 导航区域
+			$args = array(
+				'hide_empty'=>0,
+				'title_li'=>"", 
+				'depth'=>1 
+			);
+			wp_list_categories( $args ); 
+		?>
+	</div>
+</div>
+<?php
