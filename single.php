23dd89762f59c12c74b70576913bd41fc3153916
yanwu <yanwu@qinsilk.com>
2020-10-20 22:07:29 +0800

    优化文章详情显示

--- single.php
+++ single.php
@@ -66,179 +66,158 @@
 </div>
 <div class="single-detail-content-box">
 	<div class="single-detail-content">
-		<!-- 以下代码用于代码模式编辑文章 不再页面上显示 -->
-		<div style="overflow: hidden;margin: 0 15px;padding-bottom: 100px;">
-			<input type="radio" name="postdetail" id="postList" checked='true'>
-			<label for="postList" class="post-detail-label">课程目录</label>
-			<span class="post-detail-line"></span>
-			<input type="radio" name="postdetail" id="postDownload">
-			<label for="postDownload" class="post-detail-label">下载地址</label>
-			<div class="post-detaillist-content">
-				<div class="post-list-content" id="postListContent">
-					<div class="post-detail-list-item">
+		<div class="single-detail-left-content">
+			<!-- 以下代码用于代码模式编辑文章 不再页面上显示 -->
+			<!-- <div style="overflow: hidden;margin: 0 15px;padding-bottom: 100px;">
+				<input type="radio" name="postdetail" id="postList" checked='true'>
+				<label for="postList" class="post-detail-label">课程目录</label>
+				<span class="post-detail-line"></span>
+				<input type="radio" name="postdetail" id="postDownload">
+				<label for="postDownload" class="post-detail-label">下载地址</label>
+				<div class="post-detaillist-content">
+					<div class="post-list-content" id="postListContent">
+						<div class="post-detail-list-item">
+							<ul>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第1节 - 01-JavaScript</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第2节 - 02-面向对象编程介绍</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第3节 - 03-类和对象</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第4节 - 04-创建类和生成实例</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第5节 - 05-类中添加共有方法</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第6节 - 06-类继承extends和super关键字</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第7节 - 07-super调用父类普通函数以及继承中属性方法查找原则</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第8节 - 08-super必须放到子类this之前</span>
+								</li>
+								<li>
+									<span><span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第9节 - 09-使用类2个注意点</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第10节 - 10-类里面this指向问题</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第11节 - 11-面向对象tab栏-思路分析以及布局介绍</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第12节 - 12-面向对象tab栏-模块划分</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第13节 - 13-面向对象tab栏-切换功能模块</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第14节 - 14-面向对象tab栏-添加功能模块(上)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第15节 - 15-面向对象tab栏-添加功能模块(中)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第16节 - 16-面向对象tab栏-添加功能模块(下)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第17节 - 17-面向对象tab栏-删除功能模块(上)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第18节 - 18-面向对象tab栏-删除功能模块(中)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第19节 - 19-面向对象tab栏-删除功能模块(下)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第20节 - 20-面向对象tab栏-编辑功能模块(上)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第21节 - 21-面向对象tab栏-编辑功能模块(中)</span>
+								</li>
+								<li>
+									<span class="iconfont icon-Bookmark"></span>
+									<span class="pl10">第22节 - 22-面向对象tab栏-编辑功能模块(下)</span>
+								</li>
+							</ul>
+						</div>
+						<div class="post-detail-list-item-more">
+							<span id="postDetailMoreBtn">更多章节请下载完整视频观看 ＞＞</span>
+						</div>
+					</div>
+					<div class="post-list-content" id="postDownloadContent">
 						<ul>
 							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第1节 - 计算机组成</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第2节 - Python简介</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第3节 - 课程介绍</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第4节 - 解释器的作用和分类</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第5节 - Python解释器的下载和安装</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第6节 - PyCharm的作用</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第7节 - 下载和安装PyCharm</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第8节 - 新建项目</span>
-							</li>
-							<li>
-								<span><span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第9节 - 新建书写运行文件</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第10节 - PyCharm界面设置</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第11节 - PyCharm修改解释器</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第12节 - PyCharm项目管理</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第13节 - PyCharm总结</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第14节 - 注释</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第15节 - 变量的作用</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第16节 - 定义变量</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第17节 - 使用变量</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第18节 - 认识bug</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第19节 - Debug工具</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第20节 - 认识数据类型开始学习</span>
-							</li>
-							<li>
-								<span class="iconfont icon-Bookmark"></span>
-								<span class="pl10">第21节 - 变量章节总结</span>
+								<div class="post-detail-download-item-icon">
+									<span class="iconfont icon-Group-" style="font-size: 28px;"></span>							
+								</div>
+								<div class="post-detail-download-item-content">
+									<a href="https://089u.com/dir/14538876-40866962-edbf84"
+										target="_blank">
+										<div class="title">点击下载</div>
+										<div class="download-link">
+											https://089u.com/dir/14538876-40866962-edbf84
+										</div>
+									</a>
+								</div>
 							</li>
 						</ul>
 					</div>
-					<div class="post-detail-list-item-more">
-						<span id="postDetailMoreBtn">更多章节请下载完整视频观看 ＞＞</span>
-					</div>
 				</div>
-				<div class="post-list-content" id="postDownloadContent">
-					<ul>
-						<li>
-							<div class="post-detail-download-item-icon">
-								<span class="iconfont icon-Group-" style="font-size: 28px;"></span>							
-							</div>
-							<div class="post-detail-download-item-content">
-								<a href="https://www.iconfont.cn/api/project/download.zip?spm=a313x.7781069.1998910419.d7"
-									target="_blank">
-									<div class="title">download.zip</div>
-									<div class="download-link">
-										https://www.iconfont.cn/api/project/download.zip?spm=a313x.7781069.1998910419.d7
-									</div>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="post-detail-download-item-icon">
-								<span class="iconfont icon-Group-" style="font-size: 28px;"></span>							
-							</div>
-							<div class="post-detail-download-item-content">
-								<a href="https://www.iconfont.cn/api/project/download.zip?spm=a313x.7781069.1998910419.d7"
-									target="_blank">
-									<div class="title">download.zip</div>
-									<div class="download-link">
-										https://www.iconfont.cn/api/project/download.zip?spm=a313x.7781069.1998910419.d7
-									</div>
-								</a>
-							</div>
-						</li>
-						<li>
-							<div class="post-detail-download-item-icon">
-								<span class="iconfont icon-Group-" style="font-size: 28px;"></span>							
-							</div>
-							<div class="post-detail-download-item-content">
-								<a href="https://www.iconfont.cn/api/project/download.zip?spm=a313x.7781069.1998910419.d7"
-									target="_blank">
-									<div class="title">download.zip</div>
-									<div class="download-link">
-										https://www.iconfont.cn/api/project/download.zip?spm=a313x.7781069.1998910419.d7
-									</div>
-								</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
+			</div> -->
+			<?php echo $post->post_content;?>
 		</div>
-		<!-- <?php echo $post->post_content;?> -->
-	</div>
-	<!-- 相关推荐 -->
-	<div>
-		<?php
-			$category = get_the_category();
-			$args = array('category' => $cat,'numberposts'=>3);
-			$myposts = get_posts( $args );
-			foreach( $myposts as $post ) :	setup_postdata($post); 
-    	?>
-        <li>
-            <a href="<?php the_permalink(); ?>">
-                <div class="post-item" style="background-image: url('<?php echo the_post_thumbnail_url(); ?>');">
-                    <div class="post-item-mask">
-                        <img class="tips-img" src="<?php echo get_template_directory_uri() . './images/sy_box3guankan.png'; ?>">
-                    </div>
-                </div>  
-			</a>
-			<h2 class="courseName">
-				<?php the_title(); ?>
-			</h2>
-		<?php 
-			endforeach;
-			wp_reset_postdata(); 
-		?>
+		<!-- 相关推荐 -->
+		<div class="single-detail-right-content">
+			<div class="recommend-title">相关推荐</div>
+			<ul>
+				<?php
+					$category = get_the_category();
+					$args = array('category' => $cat,'numberposts'=>3);
+					$myposts = get_posts( $args );
+					foreach( $myposts as $post ) :	setup_postdata($post); 
+				?>
+				<li>
+					<a href="<?php the_permalink(); ?>">
+						<div class="recommend-post-image" style="background-image: url('<?php echo the_post_thumbnail_url(); ?>');"></div>  
+					</a>
+					<h2 class="recommend-post-name">
+						<?php the_title(); ?>
+					</h2>
+				</li>
+				<?php 
+					endforeach;
+					wp_reset_postdata(); 
+				?>
+			</ul>
+		</div>
+		<div style="clear: both;"></div>
 	</div>
 </div>
