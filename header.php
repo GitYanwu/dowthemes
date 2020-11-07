2647efaeaee690fb3217e15f9ed823875344efec
yanwu <yanwu@qinsilk.com>
2020-11-07 17:25:30 +0800

    网站头部优化

--- header.php
+++ header.php
@@ -3,6 +3,17 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Expirse" content="0"><meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-control" content="no-cache"><meta http-equiv="Cache" content="no-cache">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>51视频下载站-视频教程|免费教程|IT教程|计算机教程|开发教程|学习教程-51视频下载站</title>
+    <meta name="description" content="51视频下载站-提供免费开发视频教程,包括前端、后端、移动、运维、UI等一系列免费教程">
+    <meta name="keywords" content="51视频下载站-视频教程|免费教程|前端|后端|移动端|运维|开发|UI设计|-51视频下载站">
+    <meta name="renderer" content="webkit">
+    <meta property="og:name" content="51视频下载站">
+    <meta property="og:image" content="https://logoicon-1300501914.cos.ap-nanjing.myqcloud.com/favicon.ico">
+    <link rel="icon" href="https://logoicon-1300501914.cos.ap-nanjing.myqcloud.com/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="https://logoicon-1300501914.cos.ap-nanjing.myqcloud.com/favicon.ico" type="image/x-icon">
     <?php wp_enqueue_script("jquery"); ?>
     <?php wp_head(); ?>
     <?php add_css_enqueue() ?>
